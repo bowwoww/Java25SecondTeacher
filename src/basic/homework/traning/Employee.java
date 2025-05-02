a37e7812ed630cd972b4b49c0f9e0038bf3051df
= <rabbit88823@gmail.com>
2025-05-02 11:37:37 +0800

    同步變更

---
+++ src/basic/homework/traning/Employee.java
@@ -0,0 +1,18 @@
+package basic.homework.traning;
+
+public class Employee {
+    private String name;
+
+    public Employee(String name) {
+        this.setName(name);
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+}
